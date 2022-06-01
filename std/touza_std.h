9dba048b2aea18386d161e799a24681ea8815533
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2022-06-01 11:17:55 +0900

    (std) arg improved.
    - allow null value for option if with default.
    - parse_param implemented.
    - default_vals separated from extract_vals.

--- std/touza_std.h
+++ std/touza_std.h
@@ -1,7 +1,7 @@
 /* touza_std.h - touza/std common definition */
 /* Maintainer: SAITO Fuyuki */
 /* Created: May 30 2020 */
-/* Time-stamp: <2022/01/28 08:50:29 fuyuki touza_std.h> */
+/* Time-stamp: <2022/06/16 08:02:43 fuyuki touza_std.h> */
 /* Copyright (C) 2020, 2021 */
 /*           Japan Agency for Marine-Earth Science and Technology */
 /* Licensed under the Apache License, Version 2.0 */
