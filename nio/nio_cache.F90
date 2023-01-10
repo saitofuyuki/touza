3e0cfc9357010dd2ff50488853f924d2fbb99d0e
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2023-01-10 15:43:02 +0900

    nio: cache group settlement introduced.
    - duplicated item name adjusted in cache.

--- nio/nio_cache.F90
+++ nio/nio_cache.F90
@@ -1,10 +1,10 @@
 !!!_! nio_cache.F90 - TOUZA/Nio cache-record extension
 ! Maintainer: SAITO Fuyuki
 ! Created: Nov 9 2022
-#define TIME_STAMP 'Time-stamp: <2022/12/14 22:18:09 fuyuki nio_cache.F90>'
+#define TIME_STAMP 'Time-stamp: <2023/02/15 21:23:36 fuyuki nio_cache.F90>'
 !!!_! MANIFESTO
 !
-! Copyright (C) 2022
+! Copyright (C) 2022,2023
 !           Japan Agency for Marine-Earth Science and Technology
 !
 ! Licensed under the Apache License, Version 2.0
@@ -31,7 +31,6 @@ module TOUZA_Nio_cache
   implicit none
   private
 !!!_  - public parameters
-  integer,parameter :: ncache = 16
   integer,parameter :: lax = 3
 
   integer,parameter,public :: coll_default = 0
@@ -42,7 +41,7 @@ module TOUZA_Nio_cache
   integer,parameter,public :: coll_nonum  = 4    ! plus ignore DSET FNUM DNUM
 
 !!!_  - private parameter
-  integer,parameter :: lunit = 16
+  integer,parameter :: ucache = 16
 !!!_  - types
   type var_t
      character(len=litem) :: item = ' '
@@ -70,6 +69,14 @@ module TOUZA_Nio_cache
      type(group_t),pointer :: g(:)
   end type cache_t
 !!!_  - static
+  integer,save :: cache_rev = 0
+  character(len=litem),save :: DSET_specials(9) = &
+       & (/ 'AXLOC  ', 'AXWGT  ', 'IAXLOC ', 'IAXWGT ', &
+       &    'CAXLOC ', 'CAXWGT ', 'CIAXLOC', 'CIAXWGT', &
+       &    'CCAXLOC'  /)
+  ! CCAXLOC is used in DIUR02 of official gtool, might be a bug.
+
+  character(len=4),save :: dup_sep = '~'
 !!!_  - private static
   integer,save :: init_mode = 0
   integer,save :: init_counts = 0
@@ -83,7 +90,8 @@ module TOUZA_Nio_cache
 !!!_  - public procedures
   public init, diag, finalize
   public init_group
-  public cache_scan_file, show_cache
+  public show_cache
+  public cache_scan_file, cache_settle
   public cache_store_v0
 
   public cache_t
@@ -92,15 +100,16 @@ contains
 !!!_ + common interfaces
 !!!_  & init
   subroutine init &
-       & (ierr, u, levv, mode, stdv, icomm)
+       & (ierr, u, levv, mode, stdv, icomm, sep)
     use TOUZA_Nio_std,   only: ns_init=>init, choice, get_size_bytes, KDBL
     use TOUZA_Nio_header,only: nh_init=>init
     use TOUZA_Nio_record,only: nr_init=>init
     implicit none
-    integer,intent(out)         :: ierr
-    integer,intent(in),optional :: u
-    integer,intent(in),optional :: levv, mode, stdv
-    integer,intent(in),optional :: icomm
+    integer,         intent(out)         :: ierr
+    integer,         intent(in),optional :: u
+    integer,         intent(in),optional :: levv, mode, stdv
+    integer,         intent(in),optional :: icomm
+    character(len=*),intent(in),optional :: sep
     integer lv, md, lmd
 
     ierr = 0
@@ -120,6 +129,9 @@ contains
           if (ierr.eq.0) call ns_init(ierr, u=ulog, levv=lv, mode=lmd, stdv=stdv, icomm=icomm)
           if (ierr.eq.0) call nh_init(ierr, u=ulog, levv=lv, mode=lmd)
           if (ierr.eq.0) call nr_init(ierr, u=ulog, levv=lv, mode=lmd)
+       endif
+       if (present(sep)) then
+          dup_sep = sep
        endif
        if (init_counts.eq.0) then
           continue
@@ -152,7 +164,8 @@ contains
             & (ierr, md, pkg=PACKAGE_TAG, grp=__GRP__, mdl=__MDL__, fun='diag', u=utmp, levv=lv)
        if (is_first_force(diag_counts, md)) then
           if (ierr.eq.0) then
-             if (is_msglev_normal(lv)) call msg(TIME_STAMP, __MDL__, utmp)
+             if (is_msglev_normal(lv)) call msg(TIME_STAMP, __MDL__, u=utmp)
+             if (is_msglev_normal(lv)) call msg('(''duplicate separator = '', A)', (/dup_sep/), __MDL__, utmp)
           endif
        endif
        lmd = control_deep(md)
@@ -344,7 +357,7 @@ contains
 
 !!!_  & show_group
   subroutine show_group(ierr, grp, tag, u, levv)
-    use TOUZA_Nio_std,only: choice, join_list, is_msglev_DETAIL
+    use TOUZA_Nio_std,only: choice, join_list, is_msglev_DETAIL, is_msglev_INFO
     implicit none
     integer,         intent(out)         :: ierr
     type(group_t),   intent(in)          :: grp
@@ -377,6 +390,9 @@ contains
     else
        ttmp = 'cache'
     endif
+    if (is_msglev_DETAIL(levv-1)) then
+       if (ierr.eq.0) call show_header(ierr, grp%h, tag=ttmp, u=utmp, lev=levv)
+    endif
     do jvb = 0, grp%nvar - 1, mv
        jve = min(jvb + mv, grp%nvar)
        nv = jve - jvb
@@ -405,7 +421,7 @@ contains
              endif
           endif
        enddo
-       if (is_msglev_detail(lv)) then
+       if (is_msglev_DETAIL(lv)) then
           if (ierr.eq.0) then
              do jr = 0, grp%nrec - 1
                 do jvi = 0, nv - 1
@@ -427,15 +443,19 @@ contains
 
 !!!_  & cache_scan_file
   subroutine cache_scan_file &
-       & (ierr, c, u, flag)
+       & (ierr, c, u, flag, levv)
     use TOUZA_Nio_std,only: choice, condop, is_error_match
+    use TOUZA_Nio_std,only: msg, is_msglev_DETAIL
     use TOUZA_Nio_record,only: nio_read_header, nio_skip_records
     implicit none
     integer,         intent(out)   :: ierr
     type(cache_t),   intent(inout) :: c
     integer,         intent(in)    :: u
     integer,optional,intent(in)    :: flag
+    integer,optional,intent(in)    :: levv
 
+    integer jdrec
+    integer lv
     integer jg, lg, ng, mg
     integer jv, mv, mvdef
     integer jr, mr, mrdef
@@ -447,6 +467,8 @@ contains
     integer(kind=KIOFS) :: jpos, msize
 
     ierr = 0
+    lv = choice(lev_verbose, levv)
+
     ng = 0
     mg = 8
     lg = 24
@@ -456,6 +478,7 @@ contains
     jv = -1
     newr = .FALSE.
     jg = -1
+    jdrec = 0
 
     if (ierr.eq.0) rewind(UNIT=u, IOSTAT=ierr)
     if (ierr.eq.0) allocate(grp(0:lg-1), STAT=ierr)
@@ -464,6 +487,9 @@ contains
        if (ierr.ne.0) exit
        if (ierr.eq.0) inquire(UNIT=u, POS=jpos, IOSTAT=ierr)
        if (ierr.eq.0) call nio_read_header(ierr, h, rect, u)
+       if (is_msglev_DETAIL(lv)) then
+          call msg('(''record = '', I0, 1x, I0)', (/jdrec, int(jpos-1)/), __MDL__)
+       endif
        if (ierr.eq.0) call collate_header(ierr, jg, grp, ng, h, flag)
        if (ierr.eq.0) then
           if (jg.lt.0) then
@@ -493,6 +519,9 @@ contains
              ng = ng + 1
              if (ierr.eq.0) call init_group(ierr, grp(jg))
              if (ierr.eq.0) grp(jg)%h(:) = h(:)
+             if (is_msglev_DETAIL(lv)) then
+                call msg('(''group = '', I0)', (/jg/), __MDL__)
+             endif
           endif
        endif
        if (ierr.eq.0) call nio_skip_records(ierr, 1, u, head=h, krect=rect)
@@ -503,13 +532,25 @@ contains
        if (ierr.eq.0) then
           newv = jv.lt.0
           newr = jr.lt.0
+          if (.not.newv .and. .not.newr) then
+             if (grp(jg)%o(jr, jv).ge.0) then
+                call msg('(''dup = '', I0, 1x, I0)', (/jv, jr/), __MDL__)
+                newv = .true.
+             endif
+          endif
           if (newr) then
              jr = grp(jg)%nrec
              grp(jg)%nrec = grp(jg)%nrec + 1
+             if (is_msglev_DETAIL(lv)) then
+                call msg('(''rec = '', I0)', (/jr/), __MDL__)
+             endif
           endif
           if (newv) then
              jv = grp(jg)%nvar
              grp(jg)%nvar = grp(jg)%nvar + 1
+             if (is_msglev_DETAIL(lv)) then
+                call msg('(''var = '', I0)', (/jv/), __MDL__)
+             endif
           endif
           mv = condop(jv.ge.size(grp(jg)%v), mvdef, 0)
           mr = condop(jr.ge.size(grp(jg)%d), mrdef, 0)
@@ -534,6 +575,7 @@ contains
           grp(jg)%l(jr, jv) = msize
           ! write(*, *) 'scan', ierr, jg, jr, jv, jpos, msize
        endif
+       jdrec = jdrec + 1
     enddo
     if (is_error_match(ierr, ERR_EOF)) ierr = 0
     if (ierr.eq.0) then
@@ -544,6 +586,81 @@ contains
        c%g => NULL()
     endif
   end subroutine cache_scan_file
+
+!!!_  - cache_settle
+  subroutine cache_settle(ierr, c)
+    implicit none
+    integer,      intent(out)   :: ierr
+    type(cache_t),intent(inout) :: c
+    integer jg, jvoff
+    ierr = 0
+    jvoff = 0
+    do jg = 0, c%ngrp - 1
+       call settle_group(ierr, c%g(jg), jvoff, jg)
+    enddo
+  end subroutine cache_settle
+
+!!!_  - settle_group
+  subroutine settle_group(ierr, grp, jvoff, jgrp)
+    use TOUZA_Nio_record,only: put_header_cprop
+    use TOUZA_Nio_header,only: put_item, hi_ITEM, hi_DFMT, hi_TITL1, hi_ETTL1, hi_UNIT
+    implicit none
+    integer,      intent(out)   :: ierr
+    type(group_t),intent(inout) :: grp
+    integer,      intent(inout) :: jvoff
+    integer,      intent(in)    :: jgrp
+    integer jv,  jt,  jc
+    integer jvb, jve
+    integer jrb, jre
+    integer jgb, jge
+    integer jerr
+    integer ls
+    character(len=litem*2) :: str
+
+    ierr = 0
+    ls = max(1, len_trim(dup_sep))
+    jvb = jvoff
+    jve = jvb + grp%nvar
+    if (ierr.eq.0) call put_header_cprop(ierr, grp%h, 'VARIABLES', (/jvb + 1, jve/), 1)
+    jrb = 0
+    jre = grp%nrec
+    if (ierr.eq.0) call put_header_cprop(ierr, grp%h, 'RECORDS', (/jrb + 1, jre/), 2)
+    if (ierr.eq.0) then
+       jgb = jgrp
+       jge = jgb + 1
+       call put_header_cprop(ierr, grp%h, 'GROUPS', (/jgb + 1, jge/), 3)
+    endif
+    if (ierr.eq.0) call put_item(ierr, grp%h, ' ', hi_TITL1, 0)
+    if (ierr.eq.0) call put_item(ierr, grp%h, ' ', hi_UNIT)
+    if (ierr.eq.0) call put_item(ierr, grp%h, ' ', hi_ETTL1, 0)
+    if (ierr.eq.0) then
+101    format('CACHE', I0)
+       write(str, 101) cache_rev
+       call put_item(ierr, grp%h, str, hi_ITEM)
+    endif
+    if (ierr.eq.0) then
+102    format('XA', I0)
+       write(str, 102) ucache
+       call put_item(ierr, grp%h, str, hi_DFMT)
+    endif
+201 format(A, A, I0)
+    do jv = 0, grp%nvar - 1
+       if (index(grp%v(jv)%item, dup_sep(1:ls)).gt.0) cycle
+       if (ANY(grp%v(jv)%item.eq.grp%v(jv+1:grp%nvar-1)%item)) then
+          jc = 0
+          do jt = jv + 1, grp%nvar - 1
+             if (grp%v(jv)%item.eq.grp%v(jt)%item) then
+                jc = jc + 1
+                write(grp%v(jt)%item, 201, IOSTAT=jerr) trim(grp%v(jv)%item), dup_sep(1:ls), jc
+             endif
+          enddo
+          jc = 0
+          write(str, 201, IOSTAT=jerr) trim(grp%v(jv)%item), dup_sep(1:ls), jc
+          grp%v(jv)%item = trim(str)
+       endif
+    enddo
+    jvoff = jve
+  end subroutine settle_group
 
 !!!_  - collate_header
   subroutine collate_header &
@@ -562,7 +679,9 @@ contains
     logical :: msk(nitem)
     integer j
     integer f
-    ! integer ji
+#if TEST_NIO_CACHE > 1
+    integer ji
+#endif
     ierr = 0
 
     if (present(emask)) then
@@ -621,13 +740,15 @@ contains
 
     jgrp = -1
     do j = 0, ngrp - 1
-       ! do ji = 1, nitem
-       !    if (grp(j)%h(ji).ne.head(ji)) then
-       !       write(*, *) 'collate', j, ji, &
-       !            & '[' // trim(grp(j)%h(ji)) // ']', &
-       !            & '[' // trim(head(ji)) // ']', msk(ji)
-       !    endif
-       ! enddo
+#if TEST_NIO_CACHE > 1
+       do ji = 1, nitem
+          if (grp(j)%h(ji).ne.head(ji)) then
+             write(*, *) 'collate', j, ji, &
+                  & '[' // trim(grp(j)%h(ji)) // ']', &
+                  & '[' // trim(head(ji)) // ']', msk(ji)
+          endif
+       enddo
+#endif
        if (ALL(grp(j)%h(1:nitem).eq.head(1:nitem) .or. msk(1:nitem))) then
           jgrp = j
           exit
@@ -638,6 +759,7 @@ contains
 !!!_  - new_var
   subroutine new_var &
        & (ierr, grp, jvar, head)
+    use TOUZA_Nio_std,only: choice, parse_number
     use TOUZA_Nio_record,only: get_header_cprop
     use TOUZA_Nio_header
     implicit none
@@ -648,16 +770,19 @@ contains
     integer jc
     character(len=litem) :: name
     integer irange(2)
+
     ierr = 0
     ! write(*, *) 'new_var', jvar
-    grp%v(jvar)%item =  head(hi_ITEM)
-    grp%v(jvar)%unit =  head(hi_UNIT)
-    do jc = 1, 3
-       call get_header_cprop(name, irange, head, jc)
-       grp%v(jvar)%co(jc) = name
-       grp%v(jvar)%jbgn(jc) = irange(1)
-       grp%v(jvar)%jend(jc) = irange(2)
-    enddo
+    if (ierr.eq.0) then
+       grp%v(jvar)%item =  head(hi_ITEM)
+       grp%v(jvar)%unit =  head(hi_UNIT)
+       do jc = 1, 3
+          call get_header_cprop(name, irange, head, jc)
+          grp%v(jvar)%co(jc) = trim(name)
+          grp%v(jvar)%jbgn(jc) = irange(1)
+          grp%v(jvar)%jend(jc) = irange(2)
+       enddo
+    endif
   end subroutine new_var
 
 !!!_  - new_rec
@@ -912,8 +1037,10 @@ program test_nio_cache
   if (ierr.eq.0) then
      call sus_open(ierr, u, file, ACTION='R', STATUS='O')
   endif
-  if (ierr.eq.0) call cache_scan_file(ierr, c, u)
-  if (ierr.eq.0) call show_cache(ierr, c, tag='cache', levv=+9)
+  ! if (ierr.eq.0) call cache_scan_file(ierr, c, u, coll_std, levv=+2)
+  if (ierr.eq.0) call cache_scan_file(ierr, c, u, levv=+2)
+  if (ierr.eq.0) call cache_settle(ierr, c)
+  if (ierr.eq.0) call show_cache(ierr, c, tag='cache', levv=+2)
 
   ! if (ierr.eq.0) call get_default_header(hd)
   ! ofs = -123
