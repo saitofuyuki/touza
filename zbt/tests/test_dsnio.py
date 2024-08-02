f5e4d42ca3f78e4ff355d05fbd15b3865e1b4042
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2024-08-02 22:05:19 +0900

    zbt: (dsnio) TouzaNioCoDataset introduced.
    - _add_or_dupl() replaced by NameMap procedures.
    - dimension_suite is now method, not property.
    - raise error for invalid step.
    - [SEVERE] logical dataset association for coordinates.

--- zbt/tests/test_dsnio.py
+++ zbt/tests/test_dsnio.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python3
-# Time-stamp: <2024/07/29 17:59:42 fuyuki test_dsnio.py>
+# Time-stamp: <2024/08/02 17:56:55 fuyuki test_dsnio.py>
 
 import sys
+# import textwrap as tw
 
 from zbt.dsnio import *
+from zbt.libtouza import *
 
 
 def main(argv):
     """Sample driver."""
+    test_class = TouzaNioDataset
+    diag = False
+    show = False
+    suite = False
+    while argv:
+        if argv[0][0] != '-':
+            break
+        if argv[0] == '-c':
+            test_class = TouzaNioCoDataset
+        elif argv[0] == '-d':
+            diag = True
+        elif argv[0] == '-v':
+            show = True
+        elif argv[0] == '-s':
+            suite = True
+        argv = argv[1:]
+
     for a in argv:
         print(f'##### file: {a}')
-        ds = TouzaNioDataset(a)
+        ds = test_class(a)
         print('### dataset')
         print(ds)
         print('### dimensions')
         for d, dim in ds.dimensions.items():
             print(f'{d}: {str(dim)}')
         print('### groups')
-        print(ds.groups)
-        for g in ds.groups.values():
+        if suite:
+            groups = [ds]
+        else:
+            groups = ds.groups.values()
+            print(ds.groups)
+        for g in groups:
             print(f'### group: {g.name}')
             print(g)
             print('### dimensions')
@@ -29,7 +52,14 @@ def main(argv):
             for vn, vv in g.variables.items():
                 print(f"# var:{vn} {vv.shape} {vv.dimensions_suite}")
                 print(vv)
-                # print(vv[0,1])
+                if show:
+                    print(vv[:])
+        ds.close()
+
+    if diag:
+        print()
+        print('#' * 10, 'diag')
+        diag_datasets()
 
 
 if __name__ == '__main__':
