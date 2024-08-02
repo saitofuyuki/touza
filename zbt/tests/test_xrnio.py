cd3261a6c9a22983fc150fb1fb3756b8f3e018a1
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2024-08-02 22:06:50 +0900

    zbt: (xrnio) Apply TouzaNioCoDataset for dataset.a

--- zbt/tests/test_xrnio.py
+++ zbt/tests/test_xrnio.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Time-stamp: <2024/07/29 12:54:57 fuyuki test_xrnio.py>
+# Time-stamp: <2024/08/02 20:46:47 fuyuki test_xrnio.py>
 
 import sys
 import xarray as xr
@@ -10,29 +10,43 @@ from zbt.xrnio import *
 
 def main(argv):
     """Sample driver."""
+    decode_coords = False
+    plot = False
+    while argv:
+        if argv[0][0] != '-':
+            break
+        if argv[0] == '-c':
+            decode_coords = True
+        elif argv[0] == '-P':
+            plot = True
+        argv = argv[1:]
+
     for a in argv:
         print(f'##### file: {a}')
-        xds = xr.open_dataset(a)
-        # ds = TouzaNioDataset(a)
+        xds = xr.open_dataset(a, decode_coords=decode_coords)
         print('### dataset')
         print(xds)
         for g in [xds]:
             for vn, vv in g.data_vars.items():
                 print(f"# var:{vn} {vv.shape} {type(vv)}")
                 print(vv)
-                ext = vv.shape[:-2]
-                for x in itertools.product(*tuple(range(n) for n in ext)):
-                    # x = x + plane
-                    s = ','.join([str(idx) for idx in x])
-                    print(f"# plot: {vn}[{s},:,:]")
-                    sel = vv[x]
+                if plot:
+                    ext = vv.shape[:-2]
+                    for x in itertools.product(*tuple(range(n) for n in ext)):
+                        # x = x + plane
+                        s = ','.join([str(idx) for idx in x])
+                        print(f"# plot: {vn}[{s},:,:]")
+                        sel = vv[x]
 
-                    fig, ax = plt.subplots()
-                    sel.plot(ax=ax)
-                    plt.show()
-                    # plt.draw()
-                    # fig.clear()
-                    plt.close(fig)
+                        fig, ax = plt.subplots()
+                        try:
+                            sel.plot(ax=ax)
+                        except Exception as x:
+                            print(x)
+                        plt.show()
+                        # plt.draw()
+                        # fig.clear()
+                        plt.close(fig)
 
         # print('### dimensions')
         # for d, dim in ds.dimensions.items():
