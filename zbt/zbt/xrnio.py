f0111da168800795570b39c16d90ce21d718df54
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2024-08-13 14:00:11 +0900

    zbt: (xrnio) slightly improved coordinates.
    With a new class xrMemBackendArray for record/time coordinates.

--- zbt/zbt/xrnio.py
+++ zbt/zbt/xrnio.py
@@ -47,6 +47,33 @@ class xrNioBackendArray(xr.backends.BackendArray, dsnio.TouzaNioVar):
         # pass
 
 
+class xrMemBackendArray(xr.backends.BackendArray, dsnio.TouzaMemVar):
+    """Xarray extension for TOUZA/Nio format."""
+
+    def __init__(self, *args, **kwds):
+        super().__init__(*args, **kwds)
+        # print('ba:', self.dimensions)
+        self.dims = []
+        for d in self.dimensions:
+            self.dims.append(util.tostr(d.name))
+
+    def __getitem__(self,
+                    key: xr.core.indexing.ExplicitIndexer) \
+                    -> np.typing.ArrayLike:
+        return xr.core.indexing.explicit_indexing_adapter(
+            key,
+            self.shape,
+            xr.core.indexing.IndexingSupport.BASIC,
+            self._raw_indexing_method,
+        )
+
+    def _raw_indexing_method(self, key: tuple) \
+            -> np.typing.ArrayLike:
+        arr = super().__getitem__(key)
+        return arr
+        # pass
+
+
 class xrNioBackendEntrypoint(xr.backends.BackendEntrypoint):
     """Xarray extension for TOUZA/Nio format."""
 
@@ -67,25 +94,46 @@ class xrNioBackendEntrypoint(xr.backends.BackendEntrypoint):
                      decode_times=True,
                      decode_timedelta=True,
                      decode_coords=True,
+                     decode_attrs=True,
                      **kwds):
         if decode_coords:
             ds = dsnio.TouzaNioCoDataset(filename_or_obj,
                                          cls_var=xrNioBackendArray,
+                                         cls_arr=xrMemBackendArray,
                                          **kwds)
         else:
             ds = dsnio.TouzaNioDataset(filename_or_obj,
                                        cls_var=xrNioBackendArray,
+                                       cls_arr=xrMemBackendArray,
                                        **kwds)
         vars = {}
         for v in ds.variables.values():
             vn = ds.variables.rev_map(v, sep=ds.sub)
             dims = v.dimensions_suite(group=ds)
-            # vn = v.reverse_key(ds, raw=False)
+
+            attrs = {}
+            for j, k in v.attrs():
+                a = v.getattr(k).strip()
+                if a:
+                    k = util.tostr(k)
+                    if k in ['DIVL', 'DIVS', 'DMIN', 'DMAX', ]:
+                        a = float(a)
+                    attrs[k] = a
+            if decode_attrs:
+                for src in [('units', 'UNIT'),
+                            ('long_name', 'TITL1', 'TITL2'), ]:
+                    dst = src[0]
+                    a = ''
+                    for i in src[1:]:
+                        a = a +  v.getattr(i)
+                    if a:
+                        attrs[dst] = a
+
             data = xr.core.indexing.LazilyIndexedArray(v)
-            var = xr.Variable(dims, data)
+            var = xr.Variable(dims, data, attrs=attrs)
+            # print(vn, attrs)
             vars[util.tostr(vn)] = var
 
-        # print (vars)
         return xr.Dataset(vars)
 
     def guess_can_open(self, filename_or_obj):
