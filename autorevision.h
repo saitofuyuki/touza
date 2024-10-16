3750653cafe7c07a46c77f7c709ef11d57633fbe
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2024-10-17 07:47:25 +0900

    autotools updated.

--- autorevision.h
+++ autorevision.h
@@ -3,20 +3,20 @@
 #define AUTOREVISION_H
 
 #define VCS_TYPE		'git'
-#define VCS_BASENAME	'package'
+#define VCS_BASENAME	'public'
 #define VCS_UUID		'9a261761c4ebd54bb1c70f170b6a035a99a92914'
-#define VCS_NUM			655
-#define VCS_DATE		'2024-08-15T09:03:39Z'
-#define VCS_BRANCH		'debian/base'
-#define VCS_TAG			'upstream/1.12.3.4'
+#define VCS_NUM			696
+#define VCS_DATE		'2024-10-16T22:47:25Z'
+#define VCS_BRANCH		'public'
+#define VCS_TAG			'Release-1.13'
 #define VCS_TAG_OPENPGP	' '
-#define VCS_TICK		5
+#define VCS_TICK		0
 #define VCS_EXTRA		' '
 
-#define VCS_ACTION_STAMP	'2024-08-15T09:03:39Z!saitofuyuki@jamstec.go.jp'
-#define VCS_FULL_HASH		'4c9eb0a661ecb625cb964c6753143eef811436d3'
+#define VCS_ACTION_STAMP	'2024-10-16T22:47:25Z!saitofuyuki@jamstec.go.jp'
+#define VCS_FULL_HASH		'c5a697adae7d117caf942082fdda97391bb13c42'
 #define VCS_COMMIT_OPENPGP	' '
-#define VCS_SHORT_HASH		'4c9eb0a66'
+#define VCS_SHORT_HASH		'c5a697ad'
 
 #define VCS_WC_MODIFIED		1
 
