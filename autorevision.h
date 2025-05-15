21caee35127770673532f1f45206ef5b5ebf3178
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2025-05-15 11:40:45 +0900

    autotools updated.

--- autorevision.h
+++ autorevision.h
@@ -5,18 +5,18 @@
 #define VCS_TYPE		'git'
 #define VCS_BASENAME	'public'
 #define VCS_UUID		'9a261761c4ebd54bb1c70f170b6a035a99a92914'
-#define VCS_NUM			871
-#define VCS_DATE		'2025-05-14T23:11:13Z'
+#define VCS_NUM			875
+#define VCS_DATE		'2025-05-15T02:40:45Z'
 #define VCS_BRANCH		'public'
-#define VCS_TAG			'Release-1.16'
+#define VCS_TAG			'Release-1.16.0'
 #define VCS_TAG_OPENPGP	' '
 #define VCS_TICK		0
 #define VCS_EXTRA		' '
 
-#define VCS_ACTION_STAMP	'2025-05-14T23:11:13Z!saitofuyuki@jamstec.go.jp'
-#define VCS_FULL_HASH		'09402a018c42c301ac8b2e10a88d8cacbb58104f'
+#define VCS_ACTION_STAMP	'2025-05-15T02:40:45Z!saitofuyuki@jamstec.go.jp'
+#define VCS_FULL_HASH		'153d05a70b32402db8188e20ecde85e006605574'
 #define VCS_COMMIT_OPENPGP	' '
-#define VCS_SHORT_HASH		'09402a01'
+#define VCS_SHORT_HASH		'153d05a7'
 
 #define VCS_WC_MODIFIED		1
 
