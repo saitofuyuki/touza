fd5117a28b749279b35723767a672e3bf5b347b0
SAITO Fuyuki <saitofuyuki@jamstec.go.jp>
2025-10-28 13:04:20 +0900

    autotools updated.

--- autorevision.h
+++ autorevision.h
@@ -2,21 +2,21 @@
 #ifndef AUTOREVISION_H
 #define AUTOREVISION_H
 
-#define VCS_TYPE		'git'
-#define VCS_BASENAME	'public'
-#define VCS_UUID		'9a261761c4ebd54bb1c70f170b6a035a99a92914'
-#define VCS_NUM			980
-#define VCS_DATE		'2025-08-29T05:27:34Z'
-#define VCS_BRANCH		'public'
-#define VCS_TAG			'Release-1.16.6'
-#define VCS_TAG_OPENPGP	' '
+#define VCS_TYPE		"git"
+#define VCS_BASENAME	"public"
+#define VCS_UUID		"9a261761c4ebd54bb1c70f170b6a035a99a92914"
+#define VCS_NUM			1000
+#define VCS_DATE		"2025-10-28T04:04:20Z"
+#define VCS_BRANCH		"public"
+#define VCS_TAG			"Release-1.16.9"
+#define VCS_TAG_OPENPGP	""
 #define VCS_TICK		0
-#define VCS_EXTRA		' '
+#define VCS_EXTRA		""
 
-#define VCS_ACTION_STAMP	'2025-08-29T05:27:34Z!saitofuyuki@jamstec.go.jp'
-#define VCS_FULL_HASH		'f2b29d39291b01b0aebae9885224262d94c69b61'
-#define VCS_COMMIT_OPENPGP	' '
-#define VCS_SHORT_HASH		'f2b29d39'
+#define VCS_ACTION_STAMP	"2025-10-28T04:04:20Z!saitofuyuki@jamstec.go.jp"
+#define VCS_FULL_HASH		"84c51ab112aa52dc53a16b7a84ec7f49e37b0865"
+#define VCS_COMMIT_OPENPGP	""
+#define VCS_SHORT_HASH		"84c51ab1"
 
 #define VCS_WC_MODIFIED		0
 
